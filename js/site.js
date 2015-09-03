9a7527180e6fdcc49e541222b6b52cfaa684083f
alexander <alexwww2004@gmail.com>
2015-09-03 11:48:59 +0800

    wre

--- js/site.js
+++ js/site.js
@@ -1,52 +1,75 @@
 if (window.jQuery) $(document).ready(function() {
-	if ($('.grid').length) mason();
-	if ($('#navi').length) navi ();
-	if ($('input[type=tel]').length) tel();
-	if ($('.anchor').length) anchor();
-	if ($('.owl-carousel').length) owlCarousel();
+     if ($('.owl-carousel').length) owlCarousel();
+     if ($('.owl-carousel2').length) owlCarousel2();
+     if ($('.owl-carousel3').length) owlCarousel3();
+     if ($('.menu').length) menu();
+     if ($('.form-feedback').length) form();
+     if ($('.anchor').length) link();
 });
 
 
-function mason()
+function owlCarousel()
 {
-  $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-  });
-}
-
-function navi()
-{
-	$(window).scroll(function(){
-		var distanceTop = $('#show').offset().top - $(window).height();
-		if  ($(window).scrollTop() > distanceTop)
-			$('#navi').animate({'top':'0px'},300);
-		else
-			$('#navi').stop(true).animate({'top':'-60px'},200);
+	$(".owl-carousel").owlCarousel({
+	 
+	loop:true,
+	items : 1,
+	nav:true,
+	navText: '',
+	 
 	});
 }
 
-function tel(){ 
-	$("input[type=tel]").mask("+7 (999) 999-99-99"); 
-	return false; 
+function owlCarousel2()
+{
+	$(".owl-carousel2").owlCarousel({
+	 
+	loop:true,
+	items : 4,
+	navText: '',
+	autoWidth:true,
+	margin:10,
+	});
 }
 
-function anchor (){
+function owlCarousel3()
+{
+	$(".owl-carousel3").owlCarousel({
+	 
+	loop:true,
+	items : 6,
+	navText: '',
+	autoWidth:true,
+	margin:20,
+	});
+}
+
+function menu(){ 
+	  $("#header .navi a").click(function(){
+	  $(".menu").toggleClass("active");
+	return false;
+	});		  
+	  $("#header .navi a").click(function(){
+	  $("body").toggleClass("active");
+	return false;
+	});	
+}
+
+function form(){ 
+	  $("#header .btn-appl a").click(function(){
+	  $(".form-feedback").toggleClass("active");
+	return false;
+	});		  
+	  $("#header .btn-appl a").click(function(){
+	  $("body").toggleClass("active");
+	return false;
+	});	
+}
+
+function link (){
    $('a.anchor[href^="#"]').click(function(){
         var target = $(this).attr('href');
         $('html, body').animate({scrollTop: $(target).offset().top-100}, 1600);
         return false; 
    }); 
 }
-
-
-function owlCarousel (){
-	$('.owl-carousel').owlCarousel({
-	    nav: true,
-	    loop:true,
-	    items: 1,
-	    navText: ' '
-	});
-}
-
