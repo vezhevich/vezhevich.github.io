1d2ab86511ca5bddb2edefe83a7b078e9f020561
alexander <alexwww2004@gmail.com>
2015-09-16 10:32:16 +0800

    sdf

--- js/site.js
+++ js/site.js
@@ -1,118 +1,31 @@
 if (window.jQuery) $(document).ready(function() {
-     if ($('.owl-carousel').length) owlCarousel();
-     if ($('input[type=tel]').length) tel();
-     if ($('#header').length) hbg();
-     if ($('.menu').length) menu();
-     if ($('.basket').length) card();
-     if ($('#call').length) phone();
-     if ($('#call').length) phonedown();
+	if ($('input[type=tel]').length) tel();
+	if ($('.anchor').length) anchor();
+	if ($('.g-carousel').length) owlCarousel2();
 });
 
 
-function owlCarousel()
-{
-	$(".owl-carousel").owlCarousel({
-	 
-	loop:true,
-	items : 1,
-	nav:true,
-	navText: '',
-	 
-	});
-}
 
 function tel(){ 
 	$("input[type=tel]").mask("+7 (999) 999-99-99"); 
 	return false; 
 }
 
-function hbg(){ 
-	$(window).scroll(function(){
-	    if($(this).scrollTop()>10){
-	        $('#header').addClass('bg');
-	    }
-	    else if ($(this).scrollTop()<10){
-	        $('#header').removeClass('bg');
-	    }
+function anchor (){
+   $('a.anchor[href^="#"]').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top-100}, 1600);
+        return false; 
+   }); 
+}
+
+function owlCarousel2 (){
+	$('.g-carousel').owlCarousel({
+	    nav: true,
+	    loop:true,
+	    items: 3,
+	    margin: 3,
+	    navText: ' '
 	});
 }
-
-function menu(){ 
-	 		if( 'ontouchstart' in window ){ var click = 'touchstart'; }
-	 		else { var click = 'click'; }
-
-
-	 		$('div.burger').on(click, function(){
-	 				if( !$(this).hasClass('open') ){ openMenu(); } 
-	 				else { closeMenu(); }
-	 		});	
-	 		
-
-	 		// $('div.menu ul li a').on(click, function(e){
-	 		// 	e.preventDefault();
-	 		// 	closeMenu();
-	 		// });
-
-
-	 		function openMenu(){
-	 			
-	 			$('div.menu-bg').addClass('animate');					
-
-	 			$('div.burger').addClass('open');	
-	 			$('div.x, div.z').addClass('collapse');
-	 			$('.menu').addClass('ind');
-	 			$('.menu li').addClass('animate');
-	 			
-	 			setTimeout(function(){ 
-	 				$('div.y').hide(); 
-	 				$('div.x').addClass('rotate30'); 
-	 				$('div.z').addClass('rotate150'); 
-	 			}, 70);
-	 			setTimeout(function(){
-	 				$('div.x').addClass('rotate45'); 
-	 				$('div.z').addClass('rotate135');  
-	 			}, 120);		
-	 		}
- 		
-	 		function closeMenu(){
-
-	 			$('.menu li').removeClass('animate');
-	 			setTimeout(function(){ 			
-	 				$('div.burger').removeClass('open');	
-	 				$('div.x').removeClass('rotate45').addClass('rotate30'); 
-	 				$('div.z').removeClass('rotate135').addClass('rotate150');				
-	 				$('div.menu').removeClass('ind');			
-	 				$('div.menu-bg').removeClass('animate');			
-	 				
-	 				setTimeout(function(){ 			
-	 					$('div.x').removeClass('rotate30'); 
-	 					$('div.z').removeClass('rotate150'); 			
-	 				}, 50);
-	 				setTimeout(function(){
-	 					$('div.y').show(); 
-	 					$('div.x, div.z').removeClass('collapse');
-	 				}, 70);
-	 			}, 100);													
-	 			
-	 		}
-}
-
-
-
-function card(){
-	$('.plus').click(function(){
-		$('.basket').toggleClass("animate");
-		$('.qnty').toggleClass("added"); return false;
-	});
-}
-
-function phone(){
-	$('#call').click(function(){
-		$('#call .c1').toggleClass('up1'); 
-	 	$('#call .c2').toggleClass('up2'); 
-	 	$('#call .c3').toggleClass('up3'); 
-	});	
-}
-
-
 
