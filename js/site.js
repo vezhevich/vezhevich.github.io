668b206dfac6c0b75eb9b29811c5377d456434d6
alexander <alexwww2004@gmail.com>
2015-09-18 17:00:41 +0800

    sdf

--- js/site.js
+++ js/site.js
@@ -1,31 +1,26 @@
 if (window.jQuery) $(document).ready(function() {
-	if ($('input[type=tel]').length) tel();
-	if ($('.anchor').length) anchor();
-	if ($('.g-carousel').length) owlCarousel2();
+	if ($('select.custom').length) select();
+	if ($('.m-navi').length) slidedown();
+	if ($('.gamb').length) slidedown2();
 });
 
-
-
-function tel(){ 
-	$("input[type=tel]").mask("+7 (999) 999-99-99"); 
-	return false; 
+function select(){ 
+	$('select.custom').styler();  
 }
 
-function anchor (){
-   $('a.anchor[href^="#"]').click(function(){
-        var target = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(target).offset().top-100}, 1600);
-        return false; 
-   }); 
+function slidedown()
+{
+	$(".m-navi .lv1 > dt > a").on('click', function(){
+		   $(this).toggleClass('active');
+		   $(".lv2").slideToggle();
+			return false;
+	   });
 }
 
-function owlCarousel2 (){
-	$('.g-carousel').owlCarousel({
-	    nav: true,
-	    loop:true,
-	    items: 3,
-	    margin: 3,
-	    navText: ' '
-	});
+function slidedown2()
+{
+	$(".gamb a").on('click', function(){
+		   $(".m-navi").slideToggle();
+		return false;
+	   });
 }
-
