eb56ab088bac93df25e88fa7fec4eb0a66bd350b
alexander <alexwww2004@gmail.com>
2015-09-27 22:41:13 +0800

    два

--- js/site.js
+++ js/site.js
@@ -1,31 +1,28 @@
 if (window.jQuery) $(document).ready(function() {
 	if ($('input[type=tel]').length) tel();
-	if ($('.anchor').length) anchor();
-	if ($('.g-carousel').length) owlCarousel2();
+	if ($('.history-link').length) slidedown();
+	if ($('select.custom').length) sel();
+	if ($('#wrap').length) jScrollPane();
 });
-
-
 
 function tel(){ 
 	$("input[type=tel]").mask("+7 (999) 999-99-99"); 
 	return false; 
 }
 
-function anchor (){
-   $('a.anchor[href^="#"]').click(function(){
-        var target = $(this).attr('href');
-        $('html, body').animate({scrollTop: $(target).offset().top-100}, 1600);
-        return false; 
-   }); 
-}
-
-function owlCarousel2 (){
-	$('.g-carousel').owlCarousel({
-	    nav: true,
-	    loop:true,
-	    items: 3,
-	    margin: 3,
-	    navText: ' '
+function slidedown()
+{
+	$(".history-link .link").click(function() {
+		$(this).toggleClass('active')
+		$(".history-list").slideToggle();
+		return false;
 	});
 }
 
+function sel(){ 
+	$('select.custom').styler();  
+}
+
+function jScrollPane(){
+	$('#wrap').jScrollPane();
+}
