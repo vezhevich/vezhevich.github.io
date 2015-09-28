1511d72ded7a3f246d3cf118fb93ca1fb989eb29
alexander <alexwww2004@gmail.com>
2015-09-28 13:22:36 +0800

    уц

---
+++ js/site.js
@@ -0,0 +1,30 @@
+if (window.jQuery) $(document).ready(function() {
+	if ($('input[type=tel]').length) tel();
+	if ($('.history-link').length) slidedown();
+	if ($('select.custom').length) sel();
+	if ($('#wrap').length) jScrollPane();
+});
+
+function tel(){ 
+	$("input[type=tel]").mask("+7 (999) 999-99-99"); 
+	return false; 
+}
+
+function slidedown()
+{
+	$(".history-link .link").click(function() {
+		$(this).toggleClass('active')
+		$(".history-list").slideToggle();
+		return false;
+	});
+}
+
+function sel(){ 
+	$('select.custom').styler();  
+}
+
+function jScrollPane(){
+	$('#myModal4').on('shown.bs.modal', function () {
+	$('#wrap').jScrollPane();
+	});
+}
