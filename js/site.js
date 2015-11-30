42cfa76553663a1b02fa7c5b52cabe6c2cdbdd71
alexander <alexwww2004@gmail.com>
2015-11-30 12:49:09 +0800

    вп

--- js/site.js
+++ js/site.js
@@ -1,74 +1,12 @@
 if (window.jQuery) $(document).ready(function() {
-	if ($('.popup').length) popup();
-	if ($('#carousel').length) carousel();
-	if ($('input[type=tel]').length) tel();
-	if ($('.info-room .navi').length) tabs();
-				var params = {
-		changedEl: "select.custom",
-		visRows: 5,
-		scrollArrows: true
-	}
-	
-	cuSel(params);
-	$(".cusel").each(
-	function(){
-	    var w = parseInt($(this).width()),
-	        scrollPanel = $(this).find(".cusel-scroll-pane");
-	    if(w>=scrollPanel.width())
-	    {
-	        $(this).find(".jScrollPaneContainer").width(w);
-	        scrollPanel.width(w);
-		}
-	});
+	if ($('.item-card .link').length) slidedown();
 });
 
-function popup()
+function slidedown()
 {
-
-	$('.send-popup').bind('click', function(){
-		var rel = $(this).attr('rel');
-		$('.popup.'+rel).show();
-		$('.bg-popup').show();
-
-		$('.popup.'+rel+' .close').bind('click', function(){
-			$('.popup.'+rel).hide();
-			$('.bg-popup').hide();
-			return false;
-		});
-		$('.bg-popup').bind('click', function(){
-			$('.popup').hide();
-			$('.bg-popup').hide();
-			return false;
-		});
-		return false;
-	});
-}
-
-function carousel()
-{
-	$('#carousel').bxSlider({
-		auto: false,
-		pager: true,
-		controls: false,
-		slideWidth: '1000',
-		prevText: '',
-		nextText: ''
-	});
-}
-
-function tel(){ 
-	$("input[type=tel]").mask("+7 (999) 999-99-99"); 
-	return false; 
-}
-
-function tabs()
-{
-	$('.info-room .navi a').bind('click', function(){
-		var rel = this.rel;
-		$('.info-room .tab').hide();
-		$('.info-room .tab.'+rel).show();
-		$('.info-room .navi a').removeClass('active');
-		$(this).addClass('active');
+	$(".item-card .link").click(function() {
+		$(this).toggleClass('active')
+		$('.list', $(this).parents('dt')).slideToggle();
 		return false;
 	});
 }
