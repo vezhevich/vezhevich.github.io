1ec8d3957bd4a8c1d96766a52b16d1e4b5515ef8
alexander <alexwww2004@gmail.com>
2015-12-02 23:04:03 +0800

    pdf

---
+++ js/site.js
@@ -0,0 +1,36 @@
+if (window.jQuery) $(document).ready(function() {
+	if ($('.scroll').length) jScrollPane();
+	if ($('.search a').length) show_search();
+	if ($('input[type=tel]').length) tel();
+	if ($('select.custom').length) sel();
+});
+
+function jScrollPane(){
+	$('.scroll').jScrollPane();
+}
+
+function show_search()
+{
+	$(".search a").click(function() {
+		$(this).toggleClass('active')
+		$(".search-wrapper").slideDown();
+		return false;
+	});	
+	$(".search-wrapper .close").click(function() {
+		$(".search-wrapper").slideUp();
+		return false;
+	});
+}
+
+function tel(){ 
+	$("input[type=tel]").mask("+7 (999) 999-99-99"); 
+	return false; 
+}
+
+
+function sel(){ 
+	$('select.custom').styler();  
+}
+
+
+
