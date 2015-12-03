b9cb8852acf11375f0148dc8cdb67c2bd411c952
alexander <alexwww2004@gmail.com>
2015-12-03 15:07:47 +0800

    d

--- js/site.js
+++ js/site.js
@@ -1,6 +1,7 @@
 if (window.jQuery) $(document).ready(function() {
 	if ($('.scroll').length) jScrollPane();
 	if ($('.search a').length) show_search();
+	if ($('.slidedown-link').length) slideToggle();
 	if ($('input[type=tel]').length) tel();
 	if ($('select.custom').length) sel();
 });
@@ -21,6 +22,21 @@ function show_search()
 		return false;
 	});
 }
+
+function slideToggle()
+{
+	$(".slidedown-link").click(function() {
+		$(this).addClass('active')
+		$(".slidedown-content").slideDown();
+		return false;
+	});	
+	$(".slidedown-content .del").click(function() {
+		$(".slidedown-link").removeClass('active');
+		$(".slidedown-content").slideUp();
+		return false;
+	});
+}
+
 
 function tel(){ 
 	$("input[type=tel]").mask("+7 (999) 999-99-99"); 
