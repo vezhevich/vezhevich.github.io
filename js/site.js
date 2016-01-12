e84065edc6cada269f8a1b472de6b05044d21f12
alexander <alexwww2004@gmail.com>
2016-01-12 15:42:09 +0800

    ва

--- js/site.js
+++ js/site.js
@@ -1,14 +1,10 @@
 if (window.jQuery) $(document).ready(function() {
 	if ($('.owlCarousel').length) carousel();
+	if ($('.navigation').length) slideToggle();
 	if ($('.owlCarousel2').length) carousel2();
-	if ($('.grid').length) masonry();
-	if ($('.grid2').length) masonry2();
-	if ($('.grid3').length) masonry3();
+	if ($('.owlCarousel3').length) carousel3();
 	if ($('input[type=tel]').length) tel();
-	if ($('.datepicker').length) calendar();
-	if ($('.form-reserve-fixed').length) show();
-	if ($('.navi-room').length) modal();
-	if ($('.modal header').length) fixhead();
+	if ($('.modal').length) show();
 });
 
 function carousel()
@@ -18,9 +14,29 @@ function carousel()
     loop:false,
     nav:true,
     navText: '',
-    items: 3,
+	responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:2,
+	        },
+	        321:{
+	            items:2,
+	            margin:20,
+	        }
+	    }
 	})
 }
+
+function slideToggle()
+{
+	$(".hamb").click(function() {
+		$(this).addClass('active');
+		$(".navigation").slideToggle();
+		$("header").toggleClass('active');
+		return false;
+	});	
+}
+
 
 function carousel2()
 {
@@ -30,36 +46,20 @@ function carousel2()
     nav:true,
     navText: '',
     items: 1,
+    autoHeight:true
 	})
 }
 
-function masonry()
+function carousel3()
 {
-	$('.grid').masonry({
-	  // options
-	  itemSelector: '.grid-item'
-	});
-}
-
-function masonry2()
-{
-	$('#myModal2').on('shown.bs.modal', function () {
-		$('.grid2').masonry({
-		  itemSelector: '.item',
-		  columnWidth: 1,
-		});
-
-	});
-}
-
-function masonry3()
-{
-	$('#myModal9').on('shown.bs.modal', function () {
-		$('.grid3').masonry({
-		  itemSelector: '.item',
-		  columnWidth: 1,
-		});
-
+	$('.modal').on('shown.bs.modal', function () {
+		$('.owlCarousel3').owlCarousel({
+	    loop:false,
+	    nav:true,
+	    navText: '',
+	    items: 1,
+	    autoHeight:true
+		})
 	});
 }
 
@@ -68,56 +68,15 @@ function tel(){
 	return false; 
 }
 
-function calendar(){
-	$(function(){
-	  $.datepicker.regional['ru'] = {
-	  closeText: 'Закрыть',
-	  prevText: '&#x3c;Пред',
-	  nextText: 'След&#x3e;',
-	  currentText: 'Сегодня',
-	  monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
-	  'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-	  monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
-	  'Июл','Авг','Сен','Окт','Ноя','Дек'],
-	  dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
-	  dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
-	  dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-	  dateFormat: 'dd.mm.yy',
-	  firstDay: 1,
-	  isRTL: false
-	  };
-	  $.datepicker.setDefaults($.datepicker.regional['ru']); $(".datepicker").datepicker();
-	});
-}
-
 function show()
 {
-	$(".btn-reserve").click(function() {
-		$(".form-reserve-fixed").addClass('active');
-		return false;
-	});	
-	$(".form-reserve-fixed .close").click(function() {
-		$(".form-reserve-fixed").removeClass('active');
+	$('.modal').on('shown.bs.modal', function () {
+		$("header .closed").addClass('active');
 		return false;
 	});
-}
-
-function modal()
-{
-		$('#myModal').on('shown.bs.modal', function () {
-			$(".navi-room").addClass('active');
-			return false;
-		});
-		$("#myModal").click(function() {
-		$(".navi-room").removeClass('active');
+	$("header .closed").click(function() {
+		$("header .closed").removeClass('active');
+		$('.modal').modal('hide');
 		return false;
-	});	
-}
-
-function fixhead()
-{
-	$('.modal').on('scroll', function() {
-	var threshold = 60;
-	$('.modal header').css({"top":$(this).scrollTop() + 0});
 	});
 }
