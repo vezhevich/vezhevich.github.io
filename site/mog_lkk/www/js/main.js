0faaa82adf0766d4bbae60ceb5c040a3e6b9f29a
vezhevich <alexwww2004@gmail.com>
2017-06-30 10:33:18 +0800

    ыва

--- site/mog_lkk/www/js/main.js
+++ site/mog_lkk/www/js/main.js
@@ -1,8 +1,11 @@
 if (window.jQuery) $(document).ready(function() {
   if ($('input[type=tel]').length) tel();
+
 });
 
 function tel(){ 
   $("input[type=tel]").mask("+7 999 999-9999"); 
   return false; 
 }
+
+
