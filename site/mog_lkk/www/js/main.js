c492118c4d9e5bc1aff0f3f4c594b902348d810a
vezhevich <alexwww2004@gmail.com>
2017-06-30 20:56:22 +0800

    ыва

--- site/mog_lkk/www/js/main.js
+++ site/mog_lkk/www/js/main.js
@@ -1,33 +1,26 @@
 if (window.jQuery) $(document).ready(function() {
-  // if ($('input').length) tel();
-  //   if ($('input').length) placeholder_off();
    if ($('.main-form-wrapper').length) scrollTop();
+   if ($('input').length) placeholder_off();
 
 });
 
 function scrollTop(){ 
   $('.main-form-wrapper').on( 'click', function(e){
-  	if( window.innerWidth <= 768)
+    if( window.innerWidth <= 768)
     {
-    	$('.main-wrapper').animate({
+      $('.main-wrapper').animate({
           scrollTop: $(this).offset().top
       }, 300);
     }
     });
 }
 
-// function tel(){ 
-//   $("input").mask("+7-999-999-9999"); 
-//   return false; 
-// }
-
-
-// function placeholder_off(){ 
-//   $('input').focus(function(){
-//     $(this).data('placeholder',$(this).attr('placeholder'))
-//     $(this).attr('placeholder','');
-//   });
-//   $('input,textarea').blur(function(){
-//     $(this).attr('placeholder',$(this).data('placeholder'));
-//   });  
-// }
+function placeholder_off(){ 
+  $('input').focus(function(){
+    $(this).data('placeholder',$(this).attr('placeholder'))
+    $(this).attr('placeholder','');
+  });
+  $('input,textarea').blur(function(){
+    $(this).attr('placeholder',$(this).data('placeholder'));
+  });  
+}
