f5f8da213bd9f02f902ca463b4525e3fda63d7d9
vezhevich <alexwww2004@gmail.com>
2017-06-30 18:24:29 +0800

    sd

--- site/mog_lkk/www/js/main.js
+++ site/mog_lkk/www/js/main.js
@@ -1,8 +1,20 @@
-// if (window.jQuery) $(document).ready(function() {
-//   if ($('input').length) tel();
-//     if ($('input').length) placeholder_off();
+if (window.jQuery) $(document).ready(function() {
+  // if ($('input').length) tel();
+  //   if ($('input').length) placeholder_off();
+   if ($('.main-form-wrapper').length) scrollTop();
 
-// });
+});
+
+function scrollTop(){ 
+  $('.main-form-wrapper').on( 'click', function(e){
+  	if( window.innerWidth <= 768)
+    {
+    	$('.main-wrapper').animate({
+          scrollTop: $(this).offset().top
+      }, 300);
+    }
+    });
+}
 
 // function tel(){ 
 //   $("input").mask("+7-999-999-9999"); 
