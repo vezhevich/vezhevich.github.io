0371def1d74bb2a0c0b65ff831c3e2d795ef4710
Александр Вежевич <alexwww@kr.digital>
2017-08-27 23:35:15 +0300

    sf
    sdf

--- site/makeup/www/js/site.js
+++ site/makeup/www/js/site.js
@@ -1,11 +1,3 @@
-$(window).load(function() {
-	$('.grid').masonry({
-	  // options
-	  itemSelector: '.grid-item',
-	  horizontalOrder: false,
-	  initLayout: true
-	});	
-}
 
 $("[data-fancybox]").fancybox({
 	selector : '[data-fancybox="images"]',
