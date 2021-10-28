4cbd28073732c3f6e259829c68879d716be8e2a0
vezhevich <alexwww2004@gmail.com>
2021-10-28 10:13:55 +0300

    sd

--- site/TU/js/common.js
+++ site/TU/js/common.js
@@ -649,3 +649,21 @@ $(function() {
         $('.b-about__map-kras').addClass('active');
     });
 });
+
+// lightbox
+$(function() {
+    $('.js-lightbox-single').magnificPopup({
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
+});
