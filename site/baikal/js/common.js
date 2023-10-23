d32f7c93559c167ce850a887234ed2f97dee81a7
vezhevich <alexwww2004@gmail.com>
2023-10-23 12:42:12 +0300

    add baikal

---
+++ site/baikal/js/common.js
@@ -0,0 +1,300 @@
+// tabs
+if ($(document).find('.tabs').length) {
+	var tabNavs = document.querySelectorAll(".tabs__nav-item");
+	var tabPanes = document.querySelectorAll(".tabs__pane");
+
+	for (var i = 0; i < tabNavs.length; i++) {
+
+		tabNavs[i].addEventListener("click", function (e) {
+			e.preventDefault();
+			var activeTabAttr = e.target.getAttribute("data-tab");
+
+			for (var j = 0; j < tabNavs.length; j++) {
+				var contentAttr = tabPanes[j].getAttribute("data-tab-content");
+
+				if (activeTabAttr === contentAttr) {
+					tabNavs[j].classList.add("active");
+					tabPanes[j].classList.add("active");
+				} else {
+					tabNavs[j].classList.remove("active");
+					tabPanes[j].classList.remove("active");
+				}
+			};
+		});
+	}	
+}
+
+
+// slider-tab1
+$(function () {
+	var swiper = new Swiper('.js-slider-tab1', {
+		slidesPerView: 5,
+		spaceBetween: 16,
+		allowTouchMove: false,
+		navigation: {
+			nextEl: '.swiper-button-slider-tab1.swiper-button-next',
+			prevEl: '.swiper-button-slider-tab1.swiper-button-prev',
+		}
+	});
+});
+
+// slider-tab2
+$(function () {
+	var swiper = new Swiper('.js-slider-tab2', {
+		slidesPerView: 5,
+		spaceBetween: 16,
+		allowTouchMove: false,
+		navigation: {
+			nextEl: '.swiper-button-slider-tab2.swiper-button-next',
+			prevEl: '.swiper-button-slider-tab2.swiper-button-prev',
+		}
+	});
+});
+
+// slider-tab3
+$(function () {
+	var swiper = new Swiper('.js-slider-tab3', {
+		slidesPerView: 5,
+		spaceBetween: 16,
+		allowTouchMove: false,
+		navigation: {
+			nextEl: '.swiper-button-slider-tab3.swiper-button-next',
+			prevEl: '.swiper-button-slider-tab3.swiper-button-prev',
+		}
+	});
+});
+
+// slider-tab4
+$(function () {
+	var swiper = new Swiper('.js-slider-tab4', {
+		slidesPerView: 5,
+		spaceBetween: 16,
+		allowTouchMove: false,
+		navigation: {
+			nextEl: '.swiper-button-slider-tab4.swiper-button-next',
+			prevEl: '.swiper-button-slider-tab4.swiper-button-prev',
+		}
+	});
+});
+
+// slider-tab5
+$(function () {
+	var swiper = new Swiper('.js-slider-tab5', {
+		slidesPerView: 5,
+		spaceBetween: 16,
+		allowTouchMove: false,
+		navigation: {
+			nextEl: '.swiper-button-slider-tab5.swiper-button-next',
+			prevEl: '.swiper-button-slider-tab5.swiper-button-prev',
+		}
+	});
+});
+
+// card-view-slider
+$(function () {
+	var swiper = new Swiper('.js-card-view-slider', {
+		slidesPerView: 5,
+		spaceBetween: 16,
+		navigation: {
+			nextEl: '.swiper-button-card-view.swiper-button-next',
+			prevEl: '.swiper-button-card-view.swiper-button-prev',
+		}
+	});
+});
+
+// card-view-slider2
+$(function () {
+	var swiper = new Swiper('.js-card-view-slider2', {
+		slidesPerView: 5,
+		spaceBetween: 16,
+		navigation: {
+			nextEl: '.swiper-button-card-view2.swiper-button-next',
+			prevEl: '.swiper-button-card-view2.swiper-button-prev',
+		}
+	});
+});
+
+
+// card-feedback-slider
+$(function () {
+	var swiper = new Swiper('.js-card-feedback-slider', {
+		slidesPerView: 5,
+		spaceBetween: 16,
+		allowTouchMove: false,
+		navigation: {
+			nextEl: '.swiper-button-card-feedback.swiper-button-next',
+			prevEl: '.swiper-button-card-feedback.swiper-button-prev',
+		}
+	});
+});
+
+// card-object-inner
+$(function () {
+	var swiper = new Swiper('.js-card-object-inner-slider1', {
+		slidesPerView: 1,
+		spaceBetween: 16,
+		pagination: {
+			el: ".swiper-pagination",
+		},
+	});
+});
+
+
+// card-feedback__media1
+$(function () {
+	var swiper = new Swiper('.js-card-feedback__media', {
+		spaceBetween: 8,
+		freeMode: true,
+		slidesPerView: '3',
+	});
+});
+
+// card-article
+$(function () {
+	var swiper = new Swiper('.js-card-article-slider', {
+		slidesPerView: 4,
+		spaceBetween: 16,
+		navigation: {
+			nextEl: '.swiper-button-card-article.swiper-button-next',
+			prevEl: '.swiper-button-card-article.swiper-button-prev',
+		}
+	});
+});
+
+$(function () {
+	var accordion = (function(){
+
+		var $accordion = $('.js-accordion');
+		var $accordion_header = $accordion.find('.js-accordion-header');
+		var $accordion_item = $('.js-accordion-item');
+
+		// default settings 
+		var settings = {
+			// animation speed
+			speed: 400,
+
+			// close all other accordion items if true
+			oneOpen: false
+		};
+
+		return {
+			// pass configurable object literal
+			init: function($settings) {
+				$accordion_header.on('click', function() {
+					accordion.toggle($(this));
+				});
+
+				$.extend(settings, $settings);
+
+				// ensure only one accordion is active if oneOpen is true
+				if(settings.oneOpen && $('.js-accordion-item.active').length > 1) {
+					$('.js-accordion-item.active:not(:first)').removeClass('active');
+				}
+
+				// reveal the active accordion bodies
+				$('.js-accordion-item.active').find('> .js-accordion-body').show();
+			},
+			toggle: function($this) {
+
+				if(settings.oneOpen && $this[0] != $this.closest('.js-accordion').find('> .js-accordion-item.active > .js-accordion-header')[0]) {
+					$this.closest('.js-accordion')
+						.find('> .js-accordion-item')
+						.removeClass('active')
+						.find('.js-accordion-body')
+						.slideUp()
+				}
+
+				// show/hide the clicked accordion item
+				$this.closest('.js-accordion-item').toggleClass('active');
+				$this.next().stop().slideToggle(settings.speed);
+			}
+		}
+	})();
+
+	$(document).ready(function(){
+		accordion.init({ speed: 300, oneOpen: true });
+	});
+});
+
+// Оптимизировать и сделать один код для всех дропдаунов
+// dropdown
+$(function () {
+	if ($(document).find('.js-dropdown').length) {
+		const btnMenu = document.querySelector(".js-dropdown-open");
+		const menu = document.querySelector(".js-dropdown");
+		const toggleMenu = function () {
+			menu.classList.toggle("open");
+		}
+
+		btnMenu.addEventListener("click", function (e) {
+			e.stopPropagation();
+			toggleMenu();
+		});
+
+		document.addEventListener("click", function (e) {
+			const target = e.target;
+			const its_menu = target == menu || menu.contains(target);
+			const its_btnMenu = target == btnMenu;
+			const menu_is_active = menu.classList.contains("open");
+
+			if (!its_menu && !its_btnMenu && menu_is_active) {
+				toggleMenu();
+			}
+		});	
+	}
+});
+// Оптимизировать и сделать один код для всех дропдаунов
+// header auth menu
+$(function () {
+	if ($(document).find('.dropdown-menu-auth').length) {
+		const btnMenu = document.querySelector(".js-dropdown-menu-open");
+		const menu = document.querySelector(".dropdown-menu-auth");
+		const toggleMenu = function () {
+			menu.classList.toggle("open");
+		}
+
+		btnMenu.addEventListener("click", function (e) {
+			e.stopPropagation();
+			toggleMenu();
+		});
+
+		document.addEventListener("click", function (e) {
+			const target = e.target;
+			const its_menu = target == menu || menu.contains(target);
+			const its_btnMenu = target == btnMenu;
+			const menu_is_active = menu.classList.contains("open");
+
+			if (!its_menu && !its_btnMenu && menu_is_active) {
+				toggleMenu();
+			}
+		});
+	}
+});
+
+// счетчик
+$(function () {
+	var countInput = document.getElementsByClassName('js-qnt-value');
+	var minus = document.getElementsByClassName('js-qnt-minus');
+	var plus = document.getElementsByClassName('js-qnt-plus');
+	
+	[...document.querySelectorAll('.form-search__dropdown-item')].forEach((s, i) => {
+
+		minus[i].addEventListener('click', function() {
+			if ((countInput[i].innerHTML) * 1 >= 1) {
+				countInput[i].innerHTML = (countInput[i].innerHTML) * 1 - 1;
+			}
+		});
+
+		plus[i].addEventListener('click', function() {
+			countInput[i].innerHTML = (countInput[i].innerHTML) * 1 + 1;
+		})
+	})
+});
+
+
+// select
+$(function () {
+	$('.select-custom').niceSelect();
+});
+
+
